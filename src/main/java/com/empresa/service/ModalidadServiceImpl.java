b71972aba5beb7fe11087221c0f757eb135ac127
RomaniTait2021 <i201418593@cibertec.edu.pe>
2021-10-18 23:27:55 -0500

    actidad 6

--- src/main/java/com/empresa/service/ModalidadServiceImpl.java
+++ src/main/java/com/empresa/service/ModalidadServiceImpl.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import com.empresa.entity.Modalidad;
 import com.empresa.repository.ModalidadRepository;
 
+import jdk.jfr.internal.Repository;
+
 @Service
 public class ModalidadServiceImpl implements ModalidadService {
 
@@ -16,12 +18,37 @@ public class ModalidadServiceImpl implements ModalidadService {
 
 	@Override
 	public Modalidad insertaActualizaModalidad(Modalidad obj) {
-		return repositorio.save(obj);
+		return (Modalidad) repositorio.save(obj);
 	}
 
 	@Override
 	public List<Modalidad> listaModalidad() {
 		return repositorio.findAll();
+	}
+
+	@Override
+	public List<Modalidad> listaModalidadPorNombreNumHombresNumMujeresEdadMaximaEdadMinimaSedeDeporte(String nombre,
+			int numHombres, int numMujeres, int edadMaxima, int edadMinima, String sede) {
+		return repositorio.listaModalidadPorNombreNumHombresNumMujeresEdadMaximaEdadMinimaSedeDeporte(nombre, numHombres,numMujeres,edadMaxima,edadMinima,sede);
+	}
+
+	
+	
+	public <FiltroModalidad> List<Modalidad> listaPrFiltro(FiltroModalidad filtro) {
+		
+		return null;
+	}
+
+	@Override
+	public List listaPorFiltro(Object filtro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List listamodalidadporNombrenumHombresnumMujeresedadMaxedadMinsedeDeporte(Object filtro) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
